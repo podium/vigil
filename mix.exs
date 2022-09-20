f0509508b9d8470253d3fe3d1376d46de66ccf47
alex0112 <alarsen0112@gmail.com>
2022-09-19 20:51:52 -0600

    Added hex package metatdata and formatter file

--- mix.exs
+++ mix.exs
@@ -8,8 +8,26 @@ defmodule Vigil.MixProject do
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      test_coverage: [summary: [threshold: 70]]
+      source_url: "https://github.com/podium/vigil",
+      test_coverage: [summary: [threshold: 70]],
+      description: description(),
+      package: package()
     ]
+  end
+
+  defp package do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "vigil",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/podium/vigil"}
+    ]
+  end
+
+  defp description do
+    "Disable introspection and exfiltration of GraphQL schemas"
   end
 
   # Run "mix help compile.app" to learn about applications.
